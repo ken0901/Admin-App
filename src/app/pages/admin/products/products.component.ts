acd0cb1fc7d2747e5c905fdf3cd582b74981b4d5
ken0901 <ken5260715@gmail.com>
2024-06-07 21:50:14 -0700

    Layout - Product component

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+
+  isSidePanelVisible: boolean = false;
+
+  openSidePanel() {
+    this.isSidePanelVisible = true;
+  }
+
+  closeSidePanel() {
+    this.isSidePanelVisible = false;
+  }
 
 }
