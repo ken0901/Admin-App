a8d16af1e2e2ec6c3092e882630e19e269ad227b
ken0901 <ken5260715@gmail.com>
2024-06-07 21:34:57 -0700

    login page

--- src/app/pages/admin/login/login.component.ts
+++ src/app/pages/admin/login/login.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginObj: any = {
+    username: '',
+    password: ''
+  }
+  constructor(private router: Router){}
 
+  onLogin() {
+    if(this.loginObj.username == "admin" && this.loginObj.password == "ken12") {
+      this.router.navigateByUrl('/products');
+    }else {
+      alert('Wrong Credentials');
+    }
+  }
 }
