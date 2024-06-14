4b5aae614e98c376ac8ff57caca3dd479276f281
ken0901 <ken5260715@gmail.com>
2024-06-13 20:40:32 -0700

    Ecommerce website page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './pages/admin/login/login.component';
 import { LayoutComponent } from './pages/admin/layout/layout.component';
 import { ProductsComponent } from './pages/admin/products/products.component';
 import { CategoriesComponent } from './pages/admin/categories/categories.component';
+import { LandingComponent } from './pages/website/landing/landing.component';
 
 export const routes: Routes = [
     {
@@ -13,6 +14,10 @@ export const routes: Routes = [
     {
         path:'login',
         component:LoginComponent
+    },
+    {
+        path:'shop',
+        component:LandingComponent
     },
     {
         path:'',
--- src/app/pages/website/landing/landing.component.ts
+++ src/app/pages/website/landing/landing.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
