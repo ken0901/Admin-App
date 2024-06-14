69fc170f85f120965cf159d90f7ae0101a93a618
ken0901 <ken5260715@gmail.com>
2024-06-13 20:25:51 -0700

    Category - router link and new page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/admin/login/login.component';
 import { LayoutComponent } from './pages/admin/layout/layout.component';
 import { ProductsComponent } from './pages/admin/products/products.component';
+import { CategoriesComponent } from './pages/admin/categories/categories.component';
 
 export const routes: Routes = [
     {
@@ -20,6 +21,10 @@ export const routes: Routes = [
             {
                 path:'products',
                 component: ProductsComponent
+            },
+            {
+                path:'category',
+                component: CategoriesComponent
             }
         ]
     }
